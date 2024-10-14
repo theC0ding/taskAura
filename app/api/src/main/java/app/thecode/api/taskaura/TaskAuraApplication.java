ad38e8cf5a26bbeff7aa22a4868a5e89c6d2e457
theC0ding <manneemilekitsoukou@gmail.com>
2024-10-15 00:14:07 +0200

    init: create spring boot project

---
+++ app/api/src/main/java/app/thecode/api/taskaura/TaskAuraApplication.java
@@ -0,0 +1,13 @@
+package app.thecode.api.taskaura;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskAuraApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TaskAuraApplication.class, args);
+    }
+
+}
---
+++ app/api/src/test/java/app/thecode/api/taskaura/TaskAuraApplicationTests.java
@@ -0,0 +1,13 @@
+package app.thecode.api.taskaura;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskAuraApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
